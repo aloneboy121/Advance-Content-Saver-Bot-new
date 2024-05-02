162ac1334dd4251475279c362aa45ac27814365e
Aloneboy <71880160+aloneboy121@users.noreply.github.com>
2024-05-02 13:08:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "23881274")
     API_HASH = environ.get("API_HASH", "c867262073a58ccdebcae19601c5bf5f")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6714165430:AAGd_d7_a1PFplmSJOBmz1Wbt0UW9SwyJmw") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "6662067892:AAGHRuBY4_A2Vft3VP34H1TnVA_mz5Z8azw") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://24x7repairsericecenter:24x7repairsericecenter@saveee.cbx213c.mongodb.net/")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "24x7repairsericecenter")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
 
 class temp(object): 
